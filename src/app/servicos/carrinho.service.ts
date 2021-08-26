560777745c79c2aa25af78ee39563acf0db8fb9f
Lavinya Correia <lavinya.correia01@gmail.com>
2021-08-26 16:34:48 -0300

    Style do carrinho de compras

--- src/app/servicos/carrinho.service.ts
+++ src/app/servicos/carrinho.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Carrinho } from '../models/carrinho.model';
+import { ItemCarrinho } from '../models/item-carrinho.model';
+import { Produto } from '../models/produto.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarrinhoService {
 
+  private carrinho: Carrinho = new Carrinho([]);
+
   constructor() { }
+
+  adicionar(produto: Produto){
+    
+
+    const item = new ItemCarrinho(produto, 1);
+    this.carrinho.itens.push(item);
+  }
 }
